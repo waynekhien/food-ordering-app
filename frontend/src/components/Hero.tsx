a327e25d091d7cce32d9b284741bd005649fcca7
waynekhien <khien23092003@gmail.com>
2025-09-22 14:24:42 +0700

    feat: add HomePage, Hero, and Footer components; update AppRouter and Layout

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import hero from "../assets/ChatGPT Image Sep 22, 2025, 02_16_06 PM.png";
+
+
+const Hero = () => {
+  return (
+    <div>
+      <img src={hero} className="w-full max-h-[600ox] object-cover" />
+    </div>
+  );
+};
+
+export default Hero;
