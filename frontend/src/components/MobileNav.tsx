b11a21715918ccfadf52b53028c6d850f299ef4f
waynekhien <khien23092003@gmail.com>
2025-09-23 10:19:12 +0700

    feat: implement dropdown menu for user authentication

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,23 +1,48 @@
-import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { CircleUserRound, Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
   return (
-    <Sheet >
-        <SheetTrigger>
-            <Menu className="text-red-500" />
-        </SheetTrigger>
-        <SheetContent className="space-y-3">
-            <SheetTitle>
-                <span>Welcome to KhienFood!</span>
-            </SheetTitle>
-            <Separator />
-            <SheetDescription className="flex">
-                <Button className="flex-1 font-bold bg-red-500">Log In</Button>
-            </SheetDescription>
-        </SheetContent>
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-red-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-red-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span>Welcome to KhienFood!</span>
+          )}
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={() => loginWithRedirect()}
+              className="flex-1 font-bold bg-red-500"
+            >
+              Log In
+            </Button>
+          )}
+        </SheetDescription>
+      </SheetContent>
     </Sheet>
   );
 };
