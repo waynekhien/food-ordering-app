123f8f12b5c3888330a0e80fcbfcda2abc51d3f1
waynekhien <khien23092003@gmail.com>
2025-09-23 09:42:16 +0700

    feat: auth0 setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import AppRoutes from './AppRouter.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRouter.tsx";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
