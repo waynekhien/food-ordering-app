d48ff37c65451b332edca323b15d2b02fc2e4e5f
waynekhien <khien23092003@gmail.com>
2025-09-23 16:04:21 +0700

    feat : update-user-profile api

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { createCurrentUser } from "../controllers/user.controller.js";
-import { jwtCheck } from "../middleware/auth.middleware.js";
+import { createCurrentUser, updateCurrentUser } from "../controllers/user.controller.js";
+import { jwtCheck, jwtParse } from "../middleware/auth.middleware.js";
+import { validateMyUserRequest } from "../middleware/validation.middleware.js";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, updateCurrentUser)
 
 export default router;
